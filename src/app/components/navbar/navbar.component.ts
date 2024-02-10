dc6f6fd4e11f82a59e15be44bdd3bed55e19e709
brendasutara <sutarabrenda@gmail.com>
2024-02-10 11:57:46 -0300

    SideAccordion

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,14 +2,22 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { BtnComponent } from '../btn/btn.component';
 import { OverlayModule } from '@angular/cdk/overlay';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBell, faInfoCircle, faCircleQuestion, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, BtnComponent, OverlayModule],
+  imports: [CommonModule, BtnComponent, OverlayModule, FontAwesomeModule],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
+
+  faBell = faBell;
+  faInfoCircle = faInfoCircle;
+  faCircleQuestion = faCircleQuestion;
+  faChevronDown = faChevronDown;
+
   isOpen = false;
   isOpenEspaciodeTrabajo = false;
   isOpenReciente = false;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
         'blue-p': '#505F79',
         'green-apple': '#5AAC44',
         'green-apple-light': '#61BD4F',
+      },
+      container: {
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1024px',
+          '2xl': '1536px',
+        },
       }
     },
   },
