ef92f75239a3ec4bcfac3d0ebfa1bbc7b97566be
brendasutara <sutarabrenda@gmail.com>
2024-02-23 16:42:14 -0300

    Register

--- src/app/modules/auth/components/register-form/register-form.component.ts
+++ src/app/modules/auth/components/register-form/register-form.component.ts
@@ -7,6 +7,8 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { CustomValidators } from '@utils/validators';
+import { RequestStatus } from '@models/request-status.model';
+import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-register-form',
@@ -19,26 +21,36 @@ export class RegisterFormComponent {
   form = this.formBuilder.nonNullable.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.email, Validators.required]],
-    password: ['', [Validators.minLength(6), Validators.required]],
+    password: ['', [Validators.minLength(8), Validators.required]],
     confirmPassword: ['', [Validators.required]],
   }, {
     validators: [ CustomValidators.MatchValidator('password', 'confirmPassword') ]
   });
-  status: string = 'init';
+  status: RequestStatus = 'init';
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
   register() {
     if (this.form.valid) {
       this.status = 'loading';
       const { name, email, password } = this.form.getRawValue();
-      console.log(name, email, password);
+      this.authService.register(name, email, password)
+      .subscribe({
+        next: () => {
+          this.status = 'success';
+          this.router.navigate(['/login'])
+        },
+        error: () => {
+          this.status = 'failed';
+        }
+      })
     } else {
       this.form.markAllAsTouched();
     }
